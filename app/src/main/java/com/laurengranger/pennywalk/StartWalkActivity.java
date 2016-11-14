8400069856651e55f233d9844236d23e4304b2d5
Granger <lgranger@amazon.com>
2016-11-14 08:52:54 -0800

    new android project

---
+++ app/src/main/java/com/laurengranger/pennywalk/StartWalkActivity.java
@@ -0,0 +1,13 @@
+package com.laurengranger.pennywalk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartWalkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_walk);
+    }
+}
